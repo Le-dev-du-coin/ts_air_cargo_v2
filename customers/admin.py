4f014036aa333fc36dc22a3e34acff3b1b3a5045
Salif SANOGO <sanogodev@Sanogodevs-MacBook-Pro.local>
2026-02-26 15:04:31 +0000

    feat: animate 404/500, favicon, secure admin url via env, register all models to admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+# Enregistrement automatique de tous les modèles de l'application 'customers'
+app_models = apps.get_app_config("customers").get_models()
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
