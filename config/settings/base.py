a41b964842ca66d0a473b5cfc158d02fe6fe5c5a
Salif SANOGO <sanogodev@Sanogodevs-MacBook-Pro.local>
2026-03-07 11:40:39 +0000

    chore: add logging configuration to capture tracebacks in production

--- config/settings/base.py
+++ config/settings/base.py
@@ -211,3 +211,31 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": crontab(hour=17, minute=0),
     },
 }
+# Logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
